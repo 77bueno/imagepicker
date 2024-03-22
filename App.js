d125bc1f8ea595dd64ed918f59074859014b753d
victor <victorhugomotabueno@gmail.com>
2024-03-22 09:50:07 -0300

    função de escolher foto e verificar permissão do app

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { Button, Image, StyleSheet, Text, View } from 'react-native';
 
 /* Importando os recursos da API nativa/móvel */
 import * as ImagePicker from "expo-image-picker";
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function App() {
   
@@ -13,11 +13,44 @@ export default function App() {
   /* State de checagem de permissões de uso (através de hook useCameraPermission) */
   const [status, RequestPermission] = ImagePicker.useCameraPermissions();
 
+  console.log(status);
+
+  /* Ao entrar no app, será executada a verificação de perimssões de uso  */
+  useEffect(() => {
+    /*  Esta função mostrará um popup para o usuário perguntando
+    se ele autoriza a utilização do recurso móvel (no caso, selecionar/tirar foto). */
+    async function verificaPermissoes() {
+      const cameraStatus = await ImagePicker.requestCameraPermissionsAsync();
+
+      /*  Ele dando autorização (granted), isso será armazenado
+      no state de requestPermission. */
+
+      requestPermission(cameraStatus === "granted");
+    }
+
+    verificaPermissoes();
+  }, []);
+
+  const escolherFoto = async () => {
+    const resultado = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!resultado.canceled) {
+      setFoto(resultado.assets[0].uri);
+    }
+  };
+
+  console.log(foto);
+
   return (
     <>
       <StatusBar />
       <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Button title='Escolher Foto' />
+        <Button onPress={escolherFoto} title='Escolher Foto' />
         <Image style={{ width: 300, height: 300 }} />
       </View>
     </>
