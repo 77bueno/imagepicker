4b87013a070e3bf7d350be74b1b12701f0b97245
victor <victorhugomotabueno@gmail.com>
2024-03-22 09:52:31 -0300

    Exibição da imagem após seleção

--- App.js
+++ App.js
@@ -1,67 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import { Button, Image, StyleSheet, Text, View } from 'react-native';
+import { View, StatusBar, Button, Image, Text } from "react-native";
+import { useState, useEffect } from "react";
 
 /* Importando os recursos da API nativa/móvel */
 import * as ImagePicker from "expo-image-picker";
-import { useEffect, useState } from 'react';
 
 export default function App() {
-  
   /* State tradicional para armazenar a referência da foto (quando existir) */
   const [foto, setFoto] = useState(null);
 
-  /* State de checagem de permissões de uso (através de hook useCameraPermission) */
-  const [status, RequestPermission] = ImagePicker.useCameraPermissions();
+  /* State de checagem de permissões de uso (através do hook useCameraPermission) */
+  const [status, requestPermission] = ImagePicker.useCameraPermissions();
 
   console.log(status);
 
-  /* Ao entrar no app, será executada a verificação de perimssões de uso  */
+  /* Ao entrar no app, será executada a verificação de permissões de uso */
   useEffect(() => {
-    /*  Esta função mostrará um popup para o usuário perguntando
+    /* Esta função mostrará um popup para o usuário perguntando
     se ele autoriza a utilização do recurso móvel (no caso, selecionar/tirar foto). */
     async function verificaPermissoes() {
       const cameraStatus = await ImagePicker.requestCameraPermissionsAsync();
 
-      /*  Ele dando autorização (granted), isso será armazenado
+      /* Ele dando autorização (granted), isso será armazenado
       no state de requestPermission. */
-
       requestPermission(cameraStatus === "granted");
     }
 
     verificaPermissoes();
   }, []);
 
+  /* Ao pressionar o botão, executa esta função: */
   const escolherFoto = async () => {
+    /* Acessando via ImagePicker a biblioteca 
+    para seleção de apenas imagens, com recurso de edição habilitado,
+    proporção 16,9 e qualidade total. */
     const resultado = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.Images,
       allowsEditing: true,
-      aspect: [4, 3],
+      aspect: [16, 9],
       quality: 1,
     });
 
+    /* Se o usuário não cancelar a operação, pegamos a 
+    imagem e colocamos no state */
     if (!resultado.canceled) {
       setFoto(resultado.assets[0].uri);
     }
   };
-
   console.log(foto);
 
   return (
     <>
       <StatusBar />
       <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Button onPress={escolherFoto} title='Escolher Foto' />
-        <Image style={{ width: 300, height: 300 }} />
+        <Button onPress={escolherFoto} title="Escolher foto" />
+
+        {foto ? (
+          <Image source={{ uri: foto }} style={{ width: 300, height: 300 }} />
+        ) : (
+          <Text>Sem foto!</Text>
+        )}
       </View>
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
